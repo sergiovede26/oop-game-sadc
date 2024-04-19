5f1c8e2df50d384d48e4d21b5f9e987957437952
Sergio Diaz <sadiazcaballero@gmail.com>
2024-04-19 11:12:25 +0200

    1st approach editing the player

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
-console.log('Hello Game')
+
+class Player {
+    constructor(){
+        this.positionX = 50;
+        // this.positionY = 0;
+        // this.width = 10;
+        // this.height = 10;
+
+        this.playerElm = document.getElementById("player");
+        this.playerElm.style.left = this.positionX + "vw";
+        this.playerElm.style.bottom = this.positionY + "vh";
+        this.playerElm.style.width = this.width + "vw";
+        this.playerElm.style.height = this.height + "vh";
+        
+    }
+    moveLeft(){
+        this.positionX--;
+        this.playerElm.style.left = this.positionX + "vw";
+    }
+    moveRight(){
+        this.positionX++;
+        this.playerElm.style.left = this.positionX + "vw";
+    }
+}
+
+
+const player = new Player();
+
+document.addEventListener("keydown", () => {
+    if(e.code === "ArrowLeft"){
+        player.moveLeft();
+    }else if (e.code === "ArrowRight"){
+        player.moveRight();
+    }
+    
+})
+
